2e5c5a827ab8ed497e79aa717d9801b63b876e80
deastyjuli <deasty.juliantysalim@gmail.com>
2025-05-05 13:21:35 +0700

    Profil & Penjumlahan

---
+++ example-app/resources/views/berita.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <h4>Ini adalah halaman berita</h4>
+    ID: <?php echo $id; ?><br/>
+    Title: <?php echo $title; ?><br/>
+</div>
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/', function(){
     return view('welcome');
+});
+
+Route::get("/Profil", function(){
+    return view("Profil");
+});
+
+Route::get("/berita/{id}/{title?}", function($id, $title = NULL) {
+    return view("Berita",['id' => $id, 'title' => $title]);
+});
+
+Route::get("/total/{angka1}/{angka2}/{angka3}", function($angka1, $angka2, $angka3 = 0) {
+    return view("hasil",['total' => $angka1, $angka2, $angka3 'angka1'=> $angka1, 'angka2'=> $angka2,'angka3'=> $angka3]);
 });
