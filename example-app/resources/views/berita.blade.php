17ed4cc08acfb72b1cafc19f4305bf7f72fd0a1d
deastyjuli <deasty.juliantysalim@gmail.com>
2025-06-01 21:41:05 +0700

    Delete example-app directory

--- example-app/resources/views/berita.blade.php
+++
@@ -1,5 +0,0 @@
-<div>
-    <h4>Ini adalah halaman berita</h4>
-    ID: <?php echo $id; ?><br/>
-    Title: <?php echo $title; ?><br/>
-</div>
--- example-app/resources/views/hasil.blade.php
+++
@@ -1,6 +0,0 @@
-<div>
-    Hasil Penjumlahan
-    {{angka1}} + {{angka2}} + {{angka3}} = {{total}}
-    @if ($angka3 == 0)
-    Bilangan ke 3 tidak diisi
-</div>
